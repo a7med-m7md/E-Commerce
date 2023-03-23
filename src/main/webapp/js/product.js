edb169edc6b76b62fb8aa1f109abefb0689819ca
ahmedhsultan <a.hossam@kemet.com.eg>
2023-03-23 23:57:41 +0200

    product page

--- src/main/webapp/js/product.js
+++ src/main/webapp/js/product.js
@@ -136,7 +136,7 @@ function addToPage(jsonLaptop){
         <!-- Product details -->
         <div class="col-md-5">
             <div class="product-details">
-                <h2 class="product-name">product name goes here</h2>
+                <h2 class="product-name">${laptop.name}</h2>
                 <div>
                     <div class="product-rating">
     `;
@@ -148,28 +148,9 @@ function addToPage(jsonLaptop){
     }
 
     newProduct += `
-                </div>
-                    <a class="review-link" href="#">10 Review(s) | Add your review</a>
-                </div>
                 <div>
                     <h3 class="product-price">$${laptop.price} <del class="product-old-price">$${laptop.price}/del></h3>
                     <span class="product-available">In Stock</span>
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        
-                <div class="product-options">
-                    <label>
-                        Size
-                        <select class="input-select">
-                            <option value="0">X</option>
-                        </select>
-                    </label>
-                    <label>
-                        Color
-                        <select class="input-select">
-                            <option value="0">Red</option>
-                        </select>
-                    </label>
                 </div>
         
                 <div class="add-to-cart">
@@ -184,29 +165,20 @@ function addToPage(jsonLaptop){
                     <button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
                 </div>
         
-                <ul class="product-btns">
-                    <li><a href="#"><i class="fa fa-heart-o"></i> add to wishlist</a></li>
-                    <li><a href="#"><i class="fa fa-exchange"></i> add to compare</a></li>
-                </ul>
-        
                 <ul class="product-links">
                     <li>Category:</li>
-                    <li><a href="#">Headphones</a></li>
-                    <li><a href="#">Accessories</a></li>
-                </ul>
-        
-                <ul class="product-links">
-                    <li>Share:</li>
-                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                    <li><a href="#"><i class="fa fa-envelope"></i></a></li>
-                </ul>
-        
+                    <li><a href="#">${laptop.laptopCategory}</a></li>
+                </ul>        
             </div>
         </div>
         <!-- /Product details -->
     `;
 
     container.innerHTML += newProduct;
+
+    //Add description to details tap
+    let desc = $('#desc')[0];
+    desc.innerHTML += `
+        <p>${laptop.description}</p>
+    `;
 }
