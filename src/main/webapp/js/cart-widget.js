2ce90a8b44f20eee911fba5793290fa119a70d1a
a7med-m7md <ahmed2011mohamed99@gmail.com>
2023-03-27 03:36:46 +0200

    Fix cart-widget bug

--- src/main/webapp/js/cart-widget.js
+++ src/main/webapp/js/cart-widget.js
@@ -40,17 +40,17 @@ export function updateCart() {
         var totalSum = 0;
         var totalPrice = 0;
         document.getElementById('items-num').innerText = userId.length
-        userId.forEach((product, index) => {
-            $.get(`http://localhost:${PORT}/${DOMINO}/laptop?productId=${product.productId}`, response => {
+        userId.forEach(async (product, index) => {
+            await $.get(`http://localhost:${PORT}/${DOMINO}/laptop?productId=${product.productId}`, response => {
                 // handle the response from the server here
                 let currentProduct = JSON.parse(response);
 
-                let image = btoa(String.fromCharCode.apply(null, new Uint8Array(currentProduct.imagList[0])));
+                let image = currentProduct.imagList[0];
 
                 // create a new product widget
                 var $newProduct = $("<div>", {class: "product-widget"});
                 $newProduct.append($("<div>", {class: "product-img"}).append($("<img>", {
-                    src: `data:image/png;base64,${image}`,
+                    src: image,
                     alt: ""
                 })));
                 $newProduct.append($("<div>", {class: "product-body"}).append($("<h3 class='product-name'>", {class: currentProduct.name}).append($("<a>", {href: `http://localhost:${PORT}/store/product?uuidProduct=${currentProduct.uuid}`}).text(currentProduct.name))).append($("<h4>", {class: "product-price"}).append($("<span>", {class: "qty"}).text(userId[index].quantity + "x")).append("EG " + currentProduct.price)));
